ce25011ebabec5062ef0da8b919cd3e079c7e560
Acer <abrorbek.m1974@gmail.com>
2021-10-29 15:59:08 +0500

    added files

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from '@react-navigation/native'
+
+import Tabs from "./navigation/tab";
+import {Restaurant, OrderDelivery, Home} from './screens'
+
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+     <NavigationContainer>
+         <Stack.Navigator
+             screenOptions={{
+                 headerShown: false
+             }}
+             initialRouteName={'Home'}
+         >
+             <Stack.Screen name="Home" component={Tabs}/>
+             <Stack.Screen name="Restaurant" component={Restaurant}/>
+             <Stack.Screen name="OrderDelivery" component={OrderDelivery}/>
+         </Stack.Navigator>
+     </NavigationContainer>
+  );
+};
+
+export default App;
+
+
+
